9aa550a41957167f4010f303fce3dec6739fe460
Karen Wafik <karenwafik113@gmail.com>
2024-03-12 22:08:13 +0200

    profile component

--- front-end/Herfitk/src/app/app.component.ts
+++ front-end/Herfitk/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { HeaderComponent } from './Home/header/header.component';
 import { LoginComponent } from './Home/login/login.component';
 import { RegisterComponent } from './Home/register/register.component';
 import { AboutComponent } from './Home/about/about.component';
+import { CategoryComponent } from './Home/category/category.component';
+import { ViewprofileComponent } from './Profile/viewprofile/viewprofile.component';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,9 @@ import { AboutComponent } from './Home/about/about.component';
   HeaderComponent,
   LoginComponent,
   RegisterComponent,
-  AboutComponent
+  AboutComponent,
+  CategoryComponent,
+  ViewprofileComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
