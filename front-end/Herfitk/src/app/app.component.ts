eb01c9009010b7d693ab33544048d4fe369ffe5d
Abdelrahman Mohamed <143711387+Abdelrahman-Muhmed@users.noreply.github.com>
2024-03-13 22:41:29 +0200

    Update

--- front-end/Herfitk/src/app/app.component.ts
+++ front-end/Herfitk/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Home/header/header.component';
 import { LoginComponent } from './Home/login/login.component';
 import { RegisterComponent } from './Home/register/register.component';
@@ -8,15 +8,21 @@ import { AboutComponent } from './Home/about/about.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,
+  imports: [
   HeaderComponent,
   LoginComponent,
   RegisterComponent,
-  AboutComponent
+  AboutComponent,
+  RouterModule,
+  RouterLink,
+  RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Herfitk';
+
+
 }
