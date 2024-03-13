e8757379e7e59f1a8b42747d8e56a5c1375d3c98
Karen Wafik <karenwafik113@gmail.com>
2024-03-14 01:18:46 +0200

    ..

--- front-end/Herfitk/src/app/Home/category/category.component.ts
+++ front-end/Herfitk/src/app/Home/category/category.component.ts
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './category.component.css'
 })
 export class CategoryComponent {
+  slide(direction: string) {
+    const cards = Array.from(document.querySelectorAll('.card')) as HTMLElement[];
+    const cardWidth = cards[0].clientWidth;
 
+    if (direction === 'left') {
+      cards.forEach((card: HTMLElement, index: number) => {
+        const offset = index * cardWidth;
+        card.style.transform = `translateX(-${offset}px)`;
+      });
+    } else if (direction === 'right') {
+      cards.forEach((card: HTMLElement, index: number) => {
+        const offset = index * cardWidth;
+        card.style.transform = `translateX(${offset}px)`;
+      });
+    }
+  }
 }
