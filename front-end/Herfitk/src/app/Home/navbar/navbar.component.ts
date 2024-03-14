ccdfcfdf87b9ce3c68968ed50db292d2cc886435
Karen Wafik <karenwafik113@gmail.com>
2024-03-14 21:31:57 +0200

    navbar

--- front-end/Herfitk/src/app/Home/navbar/navbar.component.ts
+++ front-end/Herfitk/src/app/Home/navbar/navbar.component.ts
@@ -1,21 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,HostListener } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-   //Go To Login Page
+  //  //Go To Login Page
 
-   constructor(public router:Router){}
+  //  constructor(public router:Router){}
 
 
-  loginForm() {
-    this.router.navigate(['login']);
+  // loginForm() {
+  //   this.router.navigate(['login']);
+  // }
+
+  isSticky: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollY = window.scrollY || window.pageYOffset;
+    this.isSticky = scrollY > 0;
   }
 
 }
