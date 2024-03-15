c74d36e26f4da611ebfe0de8027fc27ee201d983
Karen Wafik <karenwafik113@gmail.com>
2024-03-15 05:51:36 +0200

    design

--- front-end/Herfitk/src/app/app.component.ts
+++ front-end/Herfitk/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild ,AfterViewInit,ElementRef} from '@angular/core';
 import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './Home/login/login.component';
 import { RegisterComponent } from './Home/register/register.component';
 import { AboutComponent } from './Home/about/about.component';
 import { ViewprofileComponent } from './Profile/viewprofile/viewprofile.component';
-import { DynamicBackgroundDirectiveDirective } from './Directives/dynamic-background-directive.directive';
 import { ContactusComponent } from './Home/contactus/contactus.component';
 import { CategoryComponent } from './Home/category/category.component';
 import { HeaderComponent } from './Home/header/header.component';
 import { NavbarComponent } from './Home/navbar/navbar.component';
+import { FooterComponent } from './Home/footer/footer.component';
 
 
 
@@ -26,15 +26,21 @@ import { NavbarComponent } from './Home/navbar/navbar.component';
   CategoryComponent,
   ViewprofileComponent,
   ContactusComponent,
-  DynamicBackgroundDirectiveDirective,
-  NavbarComponent
+  NavbarComponent,
+  FooterComponent
 
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef) {}
+  ngAfterViewInit() {
+      this.elementRef.nativeElement.ownerDocument
+          .body.style.backgroundColor = '#17191a';
+  }
+  
   title = 'Herfitk';
 
  
