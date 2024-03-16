c37666a131c039f2c56dd9dc63fbbec68be5b113
Abdelrahman Mohamed <143711387+Abdelrahman-Muhmed@users.noreply.github.com>
2024-03-16 18:59:04 +0200

    Herify-Controllor

---
+++ Herfitk/Herfitk_Dashboard/Controllers/HerifyController1.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Herfitk.API.DTO;
+using Herfitk.Core.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Herfitk_Dashboard.Controllers
+{
+    public class HerifyController : Controller
+    {
+        private readonly IGenericRepository<HerfiyReturnDto> repository;
+        private readonly IMapper mapper;
+        HerifyController(IGenericRepository<HerfiyReturnDto> repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        //Get All Herifys
+        public IActionResult Index()
+        {
+            return View(repository.GetAllAsync());
+        }
+
+
+    }
+}
