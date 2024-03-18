af2af69adc495a657dab6fd4389cd8afc835d0df
Ahmed Elassel <ahmed.mahmoud915@yahoo.com>
2024-03-18 06:56:27 +0200

    edit component

--- front-end/Herfitk/src/app/app.routes.ts
+++ front-end/Herfitk/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { LoginComponent } from './Home/login/login.component';
 import { RegisterComponent } from './Home/register/register.component';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { ViewprofileComponent } from './Profile/viewprofile/viewprofile.component';
 
 export const routes: Routes = [
 //  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path: "app" , component:AppComponent},
   {path: "login" , component:LoginComponent},
   {path: "register" , component:RegisterComponent},
+  {path:"profile" ,component:ViewprofileComponent}
 ];
 
 @NgModule({
