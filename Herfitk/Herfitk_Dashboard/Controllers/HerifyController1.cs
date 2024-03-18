78578d6c9b378cfad604f03e0673f8b4d72789d2
Abdelrahman Mohamed <143711387+Abdelrahman-Muhmed@users.noreply.github.com>
2024-03-18 02:23:15 +0200

    Herify-Dashborad

--- Herfitk/Herfitk_Dashboard/Controllers/HerifyController1.cs
+++ Herfitk/Herfitk_Dashboard/Controllers/HerifyController1.cs
@@ -1,27 +1,122 @@
 ﻿using AutoMapper;
 using Herfitk.API.DTO;
+using Herfitk.Core.Models.Data;
 using Herfitk.Core.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualBasic;
 using System.Threading.Tasks;
 
 namespace Herfitk_Dashboard.Controllers
 {
     public class HerifyController : Controller
     {
-        private readonly IGenericRepository<HerfiyReturnDto> repository;
+        private readonly IGenericRepository<Herfiy> repository;
         private readonly IMapper mapper;
-        HerifyController(IGenericRepository<HerfiyReturnDto> repository, IMapper mapper)
+        public HerifyController(IGenericRepository<Herfiy> repository, IMapper mapper)
         {
             this.repository = repository;
             this.mapper = mapper;
         }
 
         //Get All Herifys
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View(repository.GetAllAsync());
+            var herifys = await repository.GetAllAsync();
+            // Using Mapper To Transform From DTO To Model 
+           // var herifyDtos = mapper.Map<List<Herfiy>, List<HerfiyReturnDto>>((List<Herfiy>)herifys);
+           if(herifys == null)
+            {
+                return NotFound();
+            }
+            var mappedData = herifys.Select(item => mapper.Map<Herfiy, HerfiyReturnDto>(item));
+            return View(mappedData);
         }
 
+        //Get Herifys By Id 
+        public async Task<IActionResult> IndexId(int id)
+        {
+            //var herify = await repository.GetByIdAsync(id);
+            //var mappedDataId = mapper.Map<Herfiy, HerfiyReturnDto>(herify);
+            //return View(mappedDataId);
+
+            var herify = await repository.GetByIdAsync(id);
+
+            if (herify == null)
+            {
+                return NotFound(); // Return 404 Not Found if no Herfiy with the given id is found
+            }
+
+            var mappedDataId = mapper.Map<Herfiy, HerfiyReturnDto>(herify);
+            return View(mappedDataId);
+        }
+
+        //Add new Herifys 
+        public IActionResult Add()
+        {
+            return View(new HerfiyReturnDto());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(HerfiyReturnDto herfiyReturnDto)
+        {
+           
+            
+            if (herfiyReturnDto == null)
+            {
+                return NotFound();
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var newHerfiy = mapper.Map<HerfiyReturnDto, Herfiy>(herfiyReturnDto);
+            // Add the new Herfiy to the repository
+            await repository.AddAsync(newHerfiy);
+            return RedirectToAction("Index");
+
+        }
+
+        //Edite Herifys 
+        public async Task<IActionResult> Edite(int id)
+        {
+
+            var editeHerify = await repository.GetByIdAsync(id);
+            if (editeHerify == null)
+            {
+                return NotFound();
+            }
+            var editeHerifyReturnDto = mapper.Map<Herfiy, HerfiyReturnDto>(editeHerify);
+            return View(editeHerifyReturnDto);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edite(HerfiyReturnDto herfiyReturnDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var editeHerify = mapper.Map<HerfiyReturnDto, Herfiy>(herfiyReturnDto);
+            var editeHerifyReturn = await repository.UpdateAsync(editeHerify , editeHerify.Id);
+            return RedirectToAction("Index");
+
+        }
+
+        //Delete Herifys
+        public async Task<IActionResult> Delete(int id)
+        {
+          
+           var deleteHerify = await repository.DeleteAsync(id);
+            if (deleteHerify == null)
+            {
+                return NotFound();
+            }
+          return RedirectToAction("Index");
+
+        }
 
     }
 }
