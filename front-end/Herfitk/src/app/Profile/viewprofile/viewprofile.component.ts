7fee35a881ba81f25f4c0aa67f031911678e833e
Ahmed Elassel <ahmed.mahmoud915@yahoo.com>
2024-03-19 19:14:16 +0200

    editprofilecomponent

--- front-end/Herfitk/src/app/Profile/viewprofile/viewprofile.component.ts
+++ front-end/Herfitk/src/app/Profile/viewprofile/viewprofile.component.ts
@@ -1,19 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { DataSharingService } from '../../data-sharing.service';
+import { concatWith } from 'rxjs';
 
 @Component({
   selector: 'app-viewprofile',
   standalone: true,
   imports: [
     RouterModule,
-    ReactiveFormsModule
-    
-  
+    ReactiveFormsModule,
+    HttpClientModule,
+    CommonModule
+  ],
+  providers:[
+    DataSharingService
   ],
   templateUrl: './viewprofile.component.html',
   styleUrl: './viewprofile.component.css'
 })
-export class ViewprofileComponent {
+export class ViewprofileComponent implements OnInit {
+  ID=0;
+  profile:any;
+  reviews:any;
+constructor(private myid:ActivatedRoute,private service:DataSharingService){
+this.ID=myid.snapshot.params["id"];
+}
+  ngOnInit(): void {
+   this.service.getherifybyid(this.ID).subscribe({
+  next:(info)=>{
+    this.profile=info;
+    console.log(this.profile);
+   },
+   error:(err)=>{
+    console.log(err);
+   }
+  });
+  this.service.getreviewherify().subscribe({
+    next:(info)=>{
+     this.reviews=info;
+     console.log(this.reviews);
+    },
+    error:(err)=>{
+      console.log(err);
+    }
+  })
 
+  }
 }
