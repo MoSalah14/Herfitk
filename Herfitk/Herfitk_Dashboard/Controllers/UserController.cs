1d5fdb656ed79b38e5a403b1035b1dc18fce5404
Abdelrahman Mohamed <143711387+Abdelrahman-Muhmed@users.noreply.github.com>
2024-03-20 17:22:27 +0200

    Start-User & Some-Edit

---
+++ Herfitk/Herfitk_Dashboard/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Herfitk.Core.Models;
+using Herfitk.Core.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Herfitk_Dashboard.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IGenericRepository<AppUser> repository;
+        private readonly IMapper mapper;
+        public UserController(IGenericRepository<AppUser> repository , IMapper mapper)
+        {
+             this.repository = repository;
+             this.repository = repository;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
