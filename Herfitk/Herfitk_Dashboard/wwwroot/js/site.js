5d2ac723cb812e4cb7e3d6c6807b67866dc8c34f
Abdelrahman Mohamed <143711387+Abdelrahman-Muhmed@users.noreply.github.com>
2024-03-22 01:36:11 +0200

    Dashboard-Design

--- Herfitk/Herfitk_Dashboard/wwwroot/js/site.js
+++ Herfitk/Herfitk_Dashboard/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    var toggleSidebarButton = document.getElementById("toggleSidebarButton");
+    var sidebar = document.getElementById("sidebar");
+    var navbar = document.querySelector(".navbar");
+
+    toggleSidebarButton.addEventListener("click", function () {
+        sidebar.classList.toggle("sidebar-open");
+        navbar.classList.toggle("navbar-open");
+    });
+});
