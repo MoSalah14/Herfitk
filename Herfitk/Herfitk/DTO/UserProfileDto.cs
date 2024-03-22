3b60502959f3537eb2ac467ad488ab36ebc89bcf
Mohamed Salah <m.salah532@gmail.com>
2024-03-22 23:05:22 +0200

    AddUserProfile Dto

---
+++ Herfitk/Herfitk/DTO/UserProfileDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Herfitk.API.DTO
+{
+    public class UserProfileDto
+    {
+        public string DisplayName { get; set; }
+        public string? Address { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Image { get; set;}
+
+        public string? Zone { get; set; }
+        public string? Speciality { get; set; }
+    }
+}
