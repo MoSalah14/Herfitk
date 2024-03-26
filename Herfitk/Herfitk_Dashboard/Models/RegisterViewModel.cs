507c4f9118d269ad98dc3dc862a7b247557a8c14
Mohamed Salah <m.salah532@gmail.com>
2024-03-27 01:07:37 +0200

    last Update in dashboard User

--- Herfitk/Herfitk_Dashboard/Models/RegisterViewModel.cs
+++ Herfitk/Herfitk_Dashboard/Models/RegisterViewModel.cs
@@ -7,7 +7,7 @@
         public string? Address { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
         public string NationalId { get; set; }
         public int? RoleId { get; set; }
         public IFormFile? PersonalImage { get; set; }
