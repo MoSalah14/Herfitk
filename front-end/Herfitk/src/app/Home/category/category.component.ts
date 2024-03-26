29a8fb8bc96248f3d9746ea8dcae9182e0f406a4
Abdelrahman Mohamed <143711387+Abdelrahman-Muhmed@users.noreply.github.com>
2024-03-26 07:33:28 +0200

    termsOfServices-Translate

--- front-end/Herfitk/src/app/Home/category/category.component.ts
+++ front-end/Herfitk/src/app/Home/category/category.component.ts
@@ -4,17 +4,18 @@ import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Router, RouterModule } from '@angular/router';
 import { DataSharingService } from '../../data-sharing.service';
+import { TranslateModule } from '@ngx-translate/core';
 declare var $: any; // Declare jQuery
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TranslateModule],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css',
 })
 export class CategoryComponent  {
-  
+
   constructor(
     private router: Router,
     private httpClient: HttpClient,
--- front-end/Herfitk/src/app/termsofservices/termsofservices.component.ts
+++ front-end/Herfitk/src/app/termsofservices/termsofservices.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-termsofservices',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './termsofservices.component.html',
   styleUrl: './termsofservices.component.css'
 })
