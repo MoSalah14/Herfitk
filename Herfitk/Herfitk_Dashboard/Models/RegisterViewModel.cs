915de4a5efdf887dab5edc615a1d686e9391c97b
Abdelrahman Mohamed <143711387+Abdelrahman-Muhmed@users.noreply.github.com>
2024-03-27 03:35:16 +0200

    Valdiation

--- Herfitk/Herfitk_Dashboard/Models/RegisterViewModel.cs
+++ Herfitk/Herfitk_Dashboard/Models/RegisterViewModel.cs
@@ -1,13 +1,26 @@
-﻿namespace Herfitk_Dashboard.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Herfitk_Dashboard.Models
 {
     public class RegisterViewModel
     {
         public int Id { get; set; }
+        [DisplayName("Name")]
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
         public string? Address { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [RegularExpression(@"^[\w-\.]+@(?:(?:gmail|outlook|yahoo)\.com)$", ErrorMessage = "Email must contain '@' and end with gmail.com, outlook.com, or yahoo.com")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"^01\d{9}$", ErrorMessage = "Phone number must start with '01' and contain 11 digits.")]
         public string PhoneNumber { get; set; }
         public string? Password { get; set; }
+        [Required(ErrorMessage = "National ID is required")]
+        [RegularExpression(@"^\d{14}$", ErrorMessage = "National ID must be exactly 14 digits")]
         public string NationalId { get; set; }
         public int? RoleId { get; set; }
         public IFormFile? PersonalImage { get; set; }
